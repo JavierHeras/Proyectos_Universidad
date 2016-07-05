d970e7d9704260a2160d03c5203548d45a9de914
Javier Heras <jehi09@hotmail.com>
2016-07-05 14:56:10 -0500

    Declaración de variables

---
+++ CompraCacao/src/CompraCacao/Compras.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CompraCacao;
+import java.util.Scanner;
+/**
+ *
+ * @author JAVIER
+ */
+public class Compras {
+    
+    public static void main(String[] args) {
+     
+        // DECLARACIÓN DE VARIABLES 
+        int cantLibras;
+        int tipoCacao;
+        //INICIALIZACIÓN DE VARIABLE
+        double valorPagar=0;
+       }
+}
